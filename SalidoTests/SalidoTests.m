93290e004b5a8db5aec4cb1244dd30a5201c4c12
Michael Jones <jones.t.michael@gmail.com>
2017-05-10 12:04:02 -0400

    Add files via upload
    Initial commit 1

---
+++ SalidoTests/SalidoTests.m
@@ -0,0 +1,39 @@
+//
+//  SalidoTests.m
+//  SalidoTests
+//
+//  Created by Mike on 5/5/17.
+//  Copyright © 2017 Michael Jones. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SalidoTests : XCTestCase
+
+@end
+
+@implementation SalidoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
